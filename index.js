2a808c7fa3053659ea0e29961b32afd680a4ab0c
Anusha Konatala <anu.konatala@gmail.com>
2018-09-02 04:07:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const driver = {name: "Sam"};
 function updateDriverWithKeyAndValue(driver, key, value){
   
   const newDriver = Object.assign({}, driver, { [key]: value });
-  console.log("newDriver = " + newDriver)
+  console.log("newDriver = " + newDriver);
   return newDriver;
 }
